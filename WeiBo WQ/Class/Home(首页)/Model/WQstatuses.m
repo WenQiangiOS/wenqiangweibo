08e5156d53a0f4c96c32f37de539506e2ddbf21a
闻强 <wenqiang@wenqiangdeMacBook-Pro.local>
2016-11-20 14:38:02 +0800

    服务器获得时间和现在时间的对比

--- WeiBo WQ/Class/Home(首页)/Model/WQstatuses.m
+++ WeiBo WQ/Class/Home(首页)/Model/WQstatuses.m
@@ -15,26 +15,54 @@
 }
 
 - (NSString *)created_at {
+    
 
   NSString * dateStr = [NSString eglishDateTransformChinaDate:_created_at];
     
     
     NSDateFormatter * fmt = [[NSDateFormatter alloc] init];
-    fmt.dateFormat = @"yyyy MM dd HH:mm:ss";
+    fmt.dateFormat = @"yyyy-MM-dd HH:mm:ss";
     
     NSDate * newDate = [fmt dateFromString:dateStr];
     
+    //比较微博发送时间和现在时间的区别方法
+    NSDateComponents * cmps = [newDate deltaWithNow];
     
-    fmt.dateFormat = @"yyyy-MM-dd HH:mm:ss";
     
-    NSString * newDateStr = [fmt stringFromDate:newDate];
+    if (newDate.isThisYear) {
+        
+        if (newDate.isToday) {
+            if (cmps.hour >=1) {//大于一小时
+                fmt.dateFormat = @"HH:mm";
+                return [fmt stringFromDate:newDate];
+            } else if (cmps.minute>=1){//大于一分钟
+                return [NSString stringWithFormat:@"%ld分钟前",cmps.minute];
+            } else {//小于60秒
+                   return @"刚刚";
+            }
+            
+        }else if (newDate.isYesterday) { // 昨天发的微博
+            
+            fmt.dateFormat = @"昨天 HH:mm";
+            return [fmt stringFromDate:newDate];
+        }else { // 前天发的微博
+            
+            fmt.dateFormat = @"MM-dd HH:mm";
+            return [fmt stringFromDate:newDate];
+        }
+    }else { // 非今年
+        fmt.dateFormat = @"yy-MM-dd HH:mm:ss";
+        return [fmt stringFromDate:newDate];
+
+    }
+    
+    
     
   
     
     
-    WQLOG(@"%@",newDateStr);
+  
    
-    return dateStr;
     
 }
 
